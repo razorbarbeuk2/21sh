e032c5f12ae3ad42b648951bfb439c394da1cde0
gbourson <gbourson@student42.fr>
2017-04-25 16:20:14 +0200

    Update

--- srcs/text_line/prompt_insert_caract.c
+++ srcs/text_line/prompt_insert_caract.c
@@ -6,7 +6,7 @@
 /*   By: gbourson <gbourson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/29 16:24:18 by RAZOR             #+#    #+#             */
-/*   Updated: 2017/04/24 18:08:25 by gbourson         ###   ########.fr       */
+/*   Updated: 2017/04/25 16:16:37 by gbourson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 void		print_character(t_data *data, char result)
 {
-	tputs(tgetstr("im", NULL), 1, ft_putchar_select);
-	ft_putchar_fd(result, data->sel->tty);
+	
 	ft_add_print_caract(data, result);
-	tputs(tgetstr("ic", NULL), 1, ft_putchar_select);
-	tputs(tgetstr("ip", NULL), 1, ft_putchar_select);
-	tputs(tgetstr("ei", NULL), 1, ft_putchar_select);
+	ft_putchar_fd(result, data->sel->tty);
 	motion_list(data, 'R');
-	print_lst_line(data, data->entry->cut_line);
+	if (data->sel->i_lst < (int)data->entry->len_line)
+		print_lst_line(data, data->entry->cut_line);
+	if (data->sel->pos[1] == (data->sel->width - 2))
+		ft_move_cursor(data, RIGHT);
 }
 
 void		exec_cmd_character(t_data *data, char result)
@@ -36,9 +36,9 @@ void		exec_cmd_character(t_data *data, char result)
 		// ft_putnbr_fd(data->sel->pos_search, data->sel->tty);
 		// ft_putchar_fd(';', data->sel->tty);
 		//****************
-		print_lst_line(data, data->entry->line);
-		// ft_putnbr_fd(data->sel->i_lst, data->sel->tty);
-		// ft_putnbr_fd((int)data->entry->len_line, data->sel->tty);
+		//print_lst_line(data, data->entry->line);
+		ft_putnbr_fd(data->sel->i_lst, data->sel->tty);
+		ft_putnbr_fd((int)data->entry->len_line, data->sel->tty);
 		// ft_putnbr_fd(data->sel->i_lst - 1, data->sel->tty);
 		// ft_putnbr_fd(data->entry->len_line, data->sel->tty);
 		//****************
@@ -66,8 +66,14 @@ void		del_one_character(t_data *data, char result)
 {
 	(void)result;
 	ft_del_print_caract(data, result);
-	ft_move_cursor(data, LEFT);
+	
 	tputs(tgoto((tgetstr("dc", NULL)), 0, 0), 0, ft_putchar_select);
+	ft_move_cursor(data, LEFT);
+	// if (data->sel->pos[1] == 0)
+	// {
+	// 	ft_move_cursor(data, LEFT);
+	// 	tputs(tgoto((tgetstr("dc", NULL)), 0, 0), 0, ft_putchar_select);
+	// }
 	//tputs(tgetstr("dm", NULL), 1, ft_putchar_select);
 	
 	//tputs(tgetstr("ed", NULL), 1, ft_putchar_select);
